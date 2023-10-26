c03fa12e54fe5f70297f3ed377b1fb3fe3dbcf59
Piotr Burzała <piotrburz@gmail.com>
2023-10-26 18:02:08 +0200

    Add project files.

---
+++ RequiemArmorAddTool/Program.cs
@@ -0,0 +1,24 @@
+using Mutagen.Bethesda;
+using Mutagen.Bethesda.Synthesis;
+using Mutagen.Bethesda.Skyrim;
+
+namespace RequiemArmorAddTool
+{
+    public class Program
+    {
+        public static async Task<int> Main(string[] args)
+        {
+            return await SynthesisPipeline.Instance
+                .AddPatch<ISkyrimMod, ISkyrimModGetter>(RunPatch)
+                .SetTypicalOpen(GameRelease.SkyrimSE, "YourPatcher.esp")
+                .Run(args);
+        }
+
+        public static void RunPatch(IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
+        {
+            foreach (IArmorGetter armor in state.LoadOrder.PriorityOrder.Armor().WinningOverrides()) { 
+                armor.
+            }
+        }
+    }
+}
